50fa1b21b2bd70d1803ccf87e4ce2b159622174b
snowmantw <snowmantw@gmail.com>
2011-11-03 18:00:13 +0800

    Fix task05 #12

--- client/main/dispatch.js
+++ client/main/dispatch.js
@@ -29,7 +29,8 @@ function dispatchInstruction(ins,params)
 		assoc_handler['linenumber'] = handlerLineNumber;
 		assoc_handler['read'] = handlerRead;
 		assoc_handler['post'] = handlerPost;
-		assoc_handler['listfile'] = handlerListFile;
+		/*assoc_handler['listfile'] = handlerListFile;*/
+		assoc_handler['list'] = handlerListFile;
 		assoc_handler['log']  = handlerLog;
 	}
 	else
